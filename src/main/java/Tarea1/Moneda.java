103bfb045426f69eb70139a7f86c2b9738549ff5
Benja <benjaminrrivassepul@gmail.com>
2026-04-20 18:56:57 -0400

    Modifica la clase Moneda de PA3P y le añade los metodos compareTo y toString

--- src/main/java/Tarea1/Moneda.java
+++ src/main/java/Tarea1/Moneda.java
@@ -1,9 +1,19 @@
 package Tarea1;
 
-abstract class Moneda {
+abstract class Moneda implements Comparable<Moneda>{
     public Moneda(){}
-    public Moneda getSerie(){
-        return this;
+    public int getSerie(){
+        return this.hashCode();
     }
     public abstract int getValor();
+
+    @Override
+    public String toString() {
+        return "valor:"+this.getValor()+"; serie:"+this.getSerie();
+    }
+
+    @Override
+    public int compareTo(Moneda o) {
+        return this.getValor()-o.getValor();
+    }
 }
