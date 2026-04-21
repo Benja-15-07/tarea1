3f2f2a5d67127ab6038b6c9cb097cff6fed4cb40
Jara <joajara2025@inf.udec.cl>
2026-04-20 22:37:01 -0400

    docs: Documentación de Expendedor

--- src/main/java/Tarea1/Expendedor.java
+++ src/main/java/Tarea1/Expendedor.java
@@ -1,17 +1,28 @@
 package Tarea1;
 
+/**
+ * La clase Expendedor maneja los procesos principales del programa.
+ * Es el encargado de recibir el dinero de un comprador, y responder apropiadamente según el producto que se pida.
+*/
 class Expendedor{
+    /** Depósitos tipo Producto para cada producto disponible diferente */
     private Deposito<Producto> coca;
     private Deposito<Producto> sprite;
     private Deposito<Producto> fanta;
     private Deposito<Producto> snickers;
     private Deposito<Producto> super8;
 
+    /** Depósito reservado para almacenar las monedas del vuelto */
     private Deposito<Moneda> monVu;
 
-    private int precio;
-    public Expendedor(int size, int precioIn){
-        this.precio = precioIn;
+    /**
+     * Método constructor que inicializa todos los depósitos
+     * Los depósitos de productos son llenados con una cantidad dada de elementos por igual,
+     * El depósito de monedas de vuelto comienza vacío.
+     *
+     * @param size El tamaño de cada depósito de productos.
+     */
+    public Expendedor(int size){
         coca = new Deposito<Producto>();
         sprite = new Deposito<Producto>();
         fanta = new Deposito<Producto>();
@@ -28,10 +39,25 @@ class Expendedor{
         monVu = new Deposito<Moneda>();
     }
 
+    /**
+     * Método principal, recibe una moneda y un tipo de producto y procesa si la moneda es suficiente para comprar el
+     * producto elegido, devuelve el producto si corresponde y entrega monedas al depósito del vuelto si el pago es
+     * mayor al precio del producto.
+     *
+     * @param dinero Moneda que el usuario usa para comprar un producto.
+     * @param type Identificador numérico del tipo de producto elegido por el usuario.
+     * @return El producto elegido por el usuario si el dinero es suficiente
+    */
     public Producto comprarProducto(Moneda dinero, int type){
+        /** La mayoría de la lógica sólo se ejecuta si hay dinero en primer lugar.*/
         if(dinero != null) {
+            /**
+             * Si hay dinero, se asigna el depósito dep del cual extraer
+             * y se asigna el elemento productoActual del enum al que corresponde el producto deseado
+            */
             Deposito<Producto> dep;
             Precios productoActual;
+
             if (type == Precios.COCACOLA.getID()) {
                 dep = coca;
                 productoActual = Precios.COCACOLA;
@@ -57,8 +83,16 @@ class Expendedor{
                 productoActual = null;
             }
 
+            /**
+             * Si el tipo de producto es válido y el depósito existe
+             * y el dinero del cliente es mayor o igual al precio del producto entonces:
+            */
             if (dep != null && dinero.getValor() >= productoActual.getPrecio()) {
                 Producto auxOut = (Producto) dep.getElemento();
+                /**
+                 * Se revisa que queden productos en el depósito
+                 * Si quedan: se entrega el producto deseado y se rellena el vuelto con monedas de 100.
+                */
                 if(auxOut != null) {
                     int cantidadVuelto = (dinero.getValor() - productoActual.getPrecio()) / 100;
                     for (int i = 0; i < cantidadVuelto; i++) {
@@ -66,20 +100,32 @@ class Expendedor{
                     }
                     return auxOut;
                 }
+                /** Si no quedan: no se entrega ningún producto y el vuelto es la misma moneda inicial*/
                 else {
                     monVu.addElemento(dinero);
                     return null;
                 }
-            } else {
+            }
+            /**
+             * Si el depósito no existe o el dinero es menor al precio del producto deseado,
+             * no se entrega ningún producto y el vuelto es la misma moneda inicial
+            */
+            else {
                 monVu.addElemento(dinero);
                 return null;
             }
         }
+        /** Si no hay dinero, no se compra ningún producto y no queda nada en el depósito del vuelto.*/
         else {
             monVu.addElemento(null);
             return null;
         }
     }
+    /**
+     * Retira y devuelve el primer elemento del depósito de vuelto.
+     *
+     * @return La primera moneda que llegó al depósito
+    */
     public Moneda getVuelto() {
         return monVu.getElemento();
     }
--- src/main/java/Tarea1/Main.java
+++ src/main/java/Tarea1/Main.java
@@ -9,7 +9,7 @@ package Tarea1;
 public class Main {
     static public void main(String[] args) {
         Moneda m1 = new Moneda1500();
-        Expendedor ex = new Expendedor(2, 500);
+        Expendedor ex = new Expendedor(2);
         Comprador c1 = new Comprador(m1, 5, ex);
         System.out.println(c1.cuantoVuelto());
         System.out.println(c1.queConsumiste());
